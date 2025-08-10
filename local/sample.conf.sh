236f09517e0b80e3d4700e685368a93ddac319e1
Satoshi Takahashi <yoronneko@users.noreply.github.com>
2025-08-10 09:39:54 +0900

    doc: note Bash requirement for ARGS arrays

--- local/sample.conf.sh
+++ local/sample.conf.sh
@@ -1,14 +1,15 @@
+# shellcheck shell=bash
 # govern.sh configuration file (*.conf.sh) located in ~/local/
 #
 # Environment variables of CMD, ARGS, and MARK should be specified in 
 # this configuration file.
 #   CMD:  the command file path to execute
-#   ARGS: the argument required for the command
+#   ARGS: the arguments required for the command (as an array)
 #   MARK: the process feature that is uniquely determind with ps command
 
 # With this example configuration, govern.sh executes:
 # ---
-#  $HOME/exec/str2str -in serial://ttyF9P -out tcpserver://:2001 -b 1
+#  $HOME/exec/str2str -in serial://ttyF9P -a "JAVGRANT_G5T NONE" -out tcpserver://:2001 -b 1
 # ---
 
 LPORT=2001
@@ -17,6 +18,6 @@ DEV=ttyF9P
 if [ ! -e /dev/$DEV ]; then echo "$0: no /dev/$DEV"; exit 1; fi
 MARK="serial://$DEV:230400"
 CMD=$HOME/exec/str2str
-ARGS="-in $MARK -out tcpsvr://:$LPORT -b 1"
+ARGS=(-in "$MARK" -a "JAVGRANT_G5T NONE" -out "tcpsvr://:$LPORT" -b 1)
 
 # EOF
